20c6aa0cdede4b54ceb958c3587ca98362cd1b24
angaddeep <angaddeep_19@yahoo.com>
2019-01-30 16:44:15 -0800

    Add files via upload

---
+++ client/src/views/ToDos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div> Hi from todos </div?
+
+</template>
+
+
+<script lang = "ts">
+import Vure from 'vue'
+import { Component } from "vue-property-decorator";
+@Component
+export class ToDos extends Vue {
+
+
+}
+
+</script>
